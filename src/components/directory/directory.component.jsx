5dc670de171ee22d88213ed7befa1fe2e38464bb
Jishan Arora <jishan.sikasu@gmail.com>
2020-05-27 00:08:59 -0500

    Initial Commit

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import './directory.styles.scss'
+import MenuItem from '../menu-item/menu-item.component'
+
+class Directory extends React.Component
+{
+    constructor(){
+        super();
+        this.state={
+            sections: [
+                {
+                    title: 'Sofas',
+                    imageUrl: 'images/sofa.jpg',
+                    id: 1,
+                    linkUrl: 'shop/sofas'
+                  },
+                  {
+                    title: 'Dinning Tables',
+                    imageUrl: '/images/dinningTable.jpg',
+                    id: 2,
+                    linkUrl: 'shop/dinning-tables'
+                  },
+                  {
+                    title: 'Office Furniture',
+                    imageUrl: '/images/officeFurniture.jpg',
+                    id: 3,
+                    linkUrl: 'shop/office-furniture'
+                  },
+                  {
+                    title: 'Home Decor',
+                    imageUrl: '/images/homeDecor.jpg',
+                    size: 'large',
+                    id: 4,
+                    linkUrl: 'shop/home-decor'
+                  },
+                  {
+                    title: 'Beds',
+                    imageUrl: '/images/beds.jpg',
+                    size: 'large',
+                    id: 5,
+                    linkUrl: 'shop/beds'
+                  }
+            ]
+        }
+    }
+
+    render(){
+        return(
+            <div className="directory-menu">
+            {this.state.sections.map(({title,imageUrl,id,size}) => (
+                <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+            ))}
+            </div>
+        )
+    }
+}
+
+export default Directory;
