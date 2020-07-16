761c09a5a44d3ab3a98da23625e3c0b52f61b708
Jishan Arora <jishan.sikasu@gmail.com>
2020-07-15 19:19:56 -0500

    Updates

---
+++ precache-manifest.353306051e2cc0c6636989ef3d6f3f9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb38e56d88b3831bd4119601751622d9",
+    "url": "/SparshFurniture/index.html"
+  },
+  {
+    "revision": "d4d067b2ad72b007b25d",
+    "url": "/SparshFurniture/static/css/main.0ccbafab.chunk.css"
+  },
+  {
+    "revision": "2b56beb11773f70dfaf4",
+    "url": "/SparshFurniture/static/js/2.10362fb4.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/SparshFurniture/static/js/2.10362fb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4d067b2ad72b007b25d",
+    "url": "/SparshFurniture/static/js/main.06e8d6a4.chunk.js"
+  },
+  {
+    "revision": "0a54b166710983f4a22b",
+    "url": "/SparshFurniture/static/js/runtime-main.5813b8fe.js"
+  }
+]);
